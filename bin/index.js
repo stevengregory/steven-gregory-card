19f7625302ea7d1628583b04c74aaea84fe32295
Steven Gregory <sgregory@gmail.com>
2019-02-24 09:49:08 -0600

    Genesis update

--- bin/index.js
+++ bin/index.js
@@ -17,14 +17,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('John Papa /'),
-  handle: chalk.cyan('Disney Fan'),
-  work: chalk.white('Web Developer and Full Stack Architect'),
-  twitter: chalk.cyan('https://twitter.com/john_papa'),
-  github: chalk.cyan('https://github.com/johnpapa'),
-  linkedin: chalk.cyan('https://linkedin.com/in/papajohn'),
-  web: chalk.cyan('https://johnpapa.net'),
-  npx: chalk.white('npx @johnpapa/card'),
+  name: chalk.white('Steven Gregory /'),
+  handle: chalk.cyan('Ninja'),
+  work: chalk.white('Web Developer'),
+  twitter: chalk.cyan('https://twitter.com/sgregory'),
+  github: chalk.cyan('https://github.com/stevengregory'),
+  linkedin: chalk.cyan('https://linkedin.com/in/stevengregory2'),
+  web: chalk.cyan('http://stevengregory.ninja'),
+  npx: chalk.white('npx @stevengregory/card'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
